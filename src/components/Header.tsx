b0b74ddd40b4abaf8be3fdce6dd5377f5c195e60
MikeMSK <1954715@gmail.com>
2022-01-25 16:51:40 +0300

    init commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className={"header"}>
+            {/*<img src="../img/rock.png" alt="logo"/>*/}
+            <img src="https://logoza.ru/img/rock.png" alt="logo"/>
+        </header>
+    )
+}

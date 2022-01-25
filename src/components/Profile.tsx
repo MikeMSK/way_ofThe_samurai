8097f71c42932013eeefcd3a65c7241ef095cb6a
MikeMSK <1954715@gmail.com>
2022-01-25 16:49:19 +0300

    init commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className={"content"}>
+            <img
+                src="https://static3.depositphotos.com/1000454/256/i/600/depositphotos_2567474-stock-photo-wide-panorama-of-french-alps.jpg"
+                alt="panorama"/>
+            Main content
+        </div>
+    );
+};
+
